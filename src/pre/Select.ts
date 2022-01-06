2d40b536978883db5eeeaf538a515bc62608eb2c
TaYaKi71751 <shantian.tw@gmail.com>
2022-01-06 11:44:40 +0900

    Add filter typeof() == 'string'

--- src/pre/Select.ts
+++ src/pre/Select.ts
@@ -5,11 +5,13 @@ const fe = Object.fromEntries;
 export function filterSelected (vaccineQuantities: Array<VaccineInfo>) {
 	const config = loadConfig();
 	const selectedVaccines = config.vaccines;
-	return vaccineQuantities.filter((vaccineQuantity) => {
-		return fe(oe(vaccineQuantity).filter(([k, v]: any) => {
+	let _a = vaccineQuantities;
+	_a = _a.filter((vaccineQuantity) => {
+		return fe(oe(vaccineQuantity).filter(([k, v]) => (typeof v == 'string')).filter(([k, v]: any) => {
 			return selectedVaccines.filter((s: string): any => {
 				return v.includes(s);
 			})[0];
 		}));
 	});
+	return _a;
 }
