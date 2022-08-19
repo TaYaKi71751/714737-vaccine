1e274025b8c37e82de9824275885ea1a49dd77ba
TaYaKi71751 <shantian.tw@gmail.com>
2022-08-19 12:57:33 +0900

    Fix length check in filterSelected -> filter

--- src/pre/Select.ts
+++ src/pre/Select.ts
@@ -1,13 +1,13 @@
 import { VaccineInfo } from '../type';
 import { loadConfig } from '../util/Config';
 const oe = Object.entries;
-const fe = Object.fromEntries;
+
 export function filterSelected (vaccineQuantities: Array<VaccineInfo>) {
 	const config = loadConfig();
 	const selectedVaccines = config.vaccines;
 	let _a = vaccineQuantities;
 	_a = _a.filter((vaccineQuantity) => {
-		return fe(
+		return (
 			oe(vaccineQuantity)
 				.filter(([k, v]) => typeof v == 'string')
 				.filter(([k, v]: any) => {
